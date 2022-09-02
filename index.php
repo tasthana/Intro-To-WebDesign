293eb7fb929a17f924d78d1b1cd5e220feda28f7
Tushar Asthana <101343079+tasthana@users.noreply.github.com>
2022-09-02 11:31:12 -0400

    Lab 1: initial setup

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE HTML>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>CS 008 Index Website</title>
+        <meta name="author" content="Tushar Asthana">
+        <meta name="description" content="A site map to all my groovy assignments for the best course at UVM.">
+        <style>
+            figure{
+                float: left; 
+                text-align: center; 
+                width: 20%;    
+            }
+            
+            img{
+                max-width: 100%;
+            }
+        </style>
+    </head>
+
+    <body>
+        <figure>
+            <img alt="Tushar Asthana circ- Summer 2022" src="images/tushar-asthana.png">
+            <figcaption>Skydiving in Algarve, Portugal.</figcaption>
+        </figure>
+
+        <h1>CS 008-<em>B</em> Fall 2022</h1>
+        <h2>Tushar Asthana - Site map</h2>
+        <p><a href="ADMIN/admin.php">My Admin Folder</a></p>
+
+        <h2>Lab one - Setting up your sitemap.</h2>
+        <p><a href="testing.php">Testing Page</a></p>
+    </body>
+</html>
