9ec68e88de7fe58d4ce7d26c8c973f2f48a89777
Tushar Asthana <101343079+tasthana@users.noreply.github.com>
2022-09-02 13:57:09 -0400

    Lab 1: adding a a testing page

---
+++ testing.php
@@ -0,0 +1,25 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <title>For sale: 2012 Ducati Streetfighter</title>
+  </head>
+  <body>
+    <h1>2012 Ducati Streetfighter 848 - $9000</h1>
+    <p>2012 Ducati Streetfighter 848, Low Miles, Lots of Upgrades. Full service history.
+    Call or text 555-4400.</p>
+    <p>year: <strong>2012</strong></p>
+    <p>make and model: <strong>Ducati Streetfighter 848</strong></p>
+    <p>condition: <strong>excellent</strong></p>
+    <p>engine: <strong>848</strong></p>
+    <p>odometer: <strong>9500</strong></p>
+    <p>paint color: <strong>yellow</strong></p>
+
+    <h1>I really want to buy a brand new version of my car, but i am a good 10 years away from that money.</h1>
+    <p>2006 Mercedes-Benz E350, 140k Miles, Upgraded music sysem, broken ac converter and stolen catalyic converter. Full service history.</p>
+
+  </body>
+</html>
+ 
