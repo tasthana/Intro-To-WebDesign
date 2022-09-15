e48e0e34d8d204ea526dc1978a6fb98631d5202c
Tushar Asthana <101343079+tasthana@users.noreply.github.com>
2022-09-15 13:27:19 -0400

    Lab 3: updated sitemap

--- index.php
+++ index.php
@@ -28,8 +28,23 @@
         <h2>Tushar Asthana - Site map</h2>
         <p><a href="ADMIN/admin.php">My Admin Folder</a></p>
 
-        <h2>Lab one - Setting up your sitemap.</h2>
-        <p><a href="testing.php">Testing Page</a></p>
+        <section class="grid-layout">
+            <h2 class="header">Lab Three - Adding a Form and More Detail</h2>
+            <section class="public-files">
+                <h3>Public Files</h3>
+                <p><a href="lab3/index.php">index.php</a></p>
+            </section>
+
+            <section class="supporting-files">
+                <h3>Supporting files</h3>
+                <p><a href="lab3/css/custom.css">custom.css</a> (if you do the extra credit) </p>
+            </section>
+
+            <section class="grader-notes">
+                <h3>Notes to grader</h3>
+                <p></p>
+            </section>
+        </section>
 
         <section class="grid-layout">
             <h2 class="header">Lab Two - Making the world a better place</h2>
@@ -47,7 +62,11 @@
                 <h3>Notes to grader</h3>
                 <p></p>
             </section>
-
         </section>
+
+        <h2>Lab one - Setting up your sitemap.</h2>
+        <p><a href="testing.php">Testing Page</a></p>
+
+        
     </body>
 </html>
