cfbb45e7bc2c9f23896b97dd90ac7d282bce8319
Tushar Asthana <101343079+tasthana@users.noreply.github.com>
2022-10-02 17:44:02 -0400

    Lab 4: updated site map

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE HTML>
 <html lang="en">
     <head>
@@ -5,23 +6,48 @@
         <title>CS 008 Index Website</title>
         <meta name="author" content="Tushar Asthana">
         <meta name="description" content="A site map to all my groovy assignments for the best course at UVM.">
-        <style>
-            figure{
-                float: right; 
-                text-align: center; 
-                width: 30%;    
+    <style>
+            /* basic CSS */
+            body{
+                margin: auto;
+                padding: 3%;
+                width: 90%;
             }
-            
+
+            figure {
+                border: thin solid darkslategray;
+                border-radius: 3%;
+                padding: 3%;
+                text-align: center;
+            }
+
+            figcaption {
+                color: #839e99;
+                font-style: italic;
+                text-align: center;
+            }
+
             img{
-                max-width: 100%;
+                border-radius: 3%;
+                max-width: 100%
             }
-        </style>
+
+            .right{
+                float: right;
+                margin-left: 3%;
+            }
+
+            .small {
+                width: 20%;
+            }
+    </style>
+
     </head>
 
     <body>
-        <figure>
+        <figure class="right small">
             <img alt="Tushar Asthana circa- Summer 2022" src="images/tushar-asthana.png">
-            <figcaption>Skydiving in Algarve, Portugal.</figcaption>
+            <figcaption>Skydiving in Algarve, Portugal</figcaption>
         </figure>
 
         <h1>CS 008-<em>B</em> Fall 2022</h1>
@@ -29,15 +55,55 @@
         <p><a href="ADMIN/admin.php">My Admin Folder</a></p>
 
         <section class="grid-layout">
+            <h2 class="header">Lab Four - Styling My Site.</h2>
+            <section class="public-files">
+                <h3>Public Files</h3>
+                <p><a href="lab4/detail.php">detail.php</a></p> 
+                <p><a href="lab4/form.php">form.php</a></p> 
+                <p><a href="lab4/index.php">index.php</a></p> 
+            </section>
+
+            <section class="supporting-files">
+                <h3>Supporting files</h3>
+                <p><a href="lab4/css/custom.css">custom.css</a> </p>
+                <p><a href="lab4/images/wireframe.png">wireframe.png</a></p>
+            </section>
+
+            <section class="grader-notes">
+                <h3>Notes to grader</h3>
+                <p></p>
+            </section>
+        </section>
+        
+        <section class="grid-layout">
             <h2 class="header">Lab Three - Adding a Form and More Detail</h2>
             <section class="public-files">
                 <h3>Public Files</h3>
+                <p><a href="lab3/detail.php">detail.php</a></p>
+                <p><a href="lab3/form.php">form.php</a></p> 
                 <p><a href="lab3/index.php">index.php</a></p>
             </section>
 
             <section class="supporting-files">
                 <h3>Supporting files</h3>
-                <p><a href="lab3/css/custom.css">custom.css</a> (if you do the extra credit) </p>
+                <p><a href="lab3/css/custom.css">custom.css</a> </p>
+                <p><a href="lab3/images/wireframe.png">wireframe.png</a></p>
+            </section>
+
+            <section class="grader-notes">
+                <h3>Notes to grader</h3>
+                <p></p>
+            </section>
+        </section>
+        <section class="grid-layout">
+            <h2 class="header">Lab Two - Making the World a Better Place</h2>
+            <section class="public-files">
+                <h3>Public Files</h3>
+                <p><a href="lab2/index.php">index.php</a></p>      
+            </section>
+
+            <section class="supporting-files">
+                <h3>Supporting files</h3>              
             </section>
 
             <section class="grader-notes">
@@ -47,15 +113,13 @@
         </section>
 
         <section class="grid-layout">
-            <h2 class="header">Lab Two - Making the world a better place</h2>
+            <h2>Lab one - Setting up my sitemap.</h2>
             <section class="public-files">
                 <h3>Public Files</h3>
-                <p><a href="lab2/index.php">index.php</a></p>
+                <p><a href="test-page.php">Testing Page</a></p>      
             </section>
-
             <section class="supporting-files">
                 <h3>Supporting files</h3>
-                <p><a href="lab2/css/custom.css">custom.css</a> (if you do the extra credit) </p>
             </section>
 
             <section class="grader-notes">
@@ -64,9 +128,5 @@
             </section>
         </section>
 
-        <h2>Lab one - Setting up your sitemap.</h2>
-        <p><a href="testing.php">Testing Page</a></p>
-
-        
     </body>
 </html>
