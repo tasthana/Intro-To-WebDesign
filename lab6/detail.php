7d95cc7f213dbd62aeeb29fe8b18f276c0d6b591
Tushar Asthana <101343079+tasthana@users.noreply.github.com>
2022-10-18 10:11:15 -0400

    Add files via upload

---
+++ lab6/detail.php
@@ -0,0 +1,73 @@
+<!DOCTYPE HTML>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title> Tennis </title>
+        <meta name="author" content="Tushar Asthana">
+        <meta name="description" content="The 4th largest followed sport in the world. Players from all over the world come to 
+        compete in tournaments to make a name for themselves. Here we will break down rankings, icons, and my favorite player! ">
+        <link rel="stylesheet" href="css/custom.css?version=<?php print time(); ?>" type="text/css">
+        <!-- Side note to myself, things come and go, take a breath. -->
+        <meta name ="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="css/custom.css?version=<?php print time(); ?>" ype="text/css">
+        <link rel="stylesheet" media= "(max-width: 800px)" href="css/custom-tablet.css?version=<?php print time(); ?>"type="text/css">
+        <link rel="stylesheet" media=" (max-width: 600px)" href="css/custom-phone.css?version=<?php print time(); ?>" type="text/ass">
+    </head>
+
+    <body>
+
+        <header>
+        <!-- logo image would go here -->
+            <h1>Wimbledon Championships</h1>
+            <h2>The Biggest Tennis Tournament in the World!</h2>
+        </header>
+
+        <nav class= "nav">
+            <a href="index.php">Home</a>
+            <a href="detail.php">Wimbledon</a>
+            <a href="form.php">Form</a>
+        </nav>
+
+        <main>
+
+            <section>
+                <h2>Origins</h2>
+                <p>The most prestigious tennis event, the Wimbledon Championships started 133 years ago and is the oldest tennis tournament on the tour. 
+                The Championships is held in the town of Wimbledon and is informally called the Wimbledon Championships. 
+                The club itself is called All England Lawn Tennis and Croquet Club, established July 23, 1868. </p>
+            </section>
+            
+            <section>
+                <h2>Ball Boys and Girls</h2>
+                <p>Aside from the referee and the players, the only people on the court are the Ball Boys and Girls.
+                 In a usual year, there are approximately 250 Ball Boys and Girls from around 1000 applicants who work at Wimbledon.
+                 The average age of a Ball person is around 15 years of age, and each person can only work in the tournament for two years. 
+                 All of the kids are recruited from local highschools, and about 80 kids are callbacks from the previous year. 
+                 Essentially, these kids are trained from Febuary to when the tournament starts if selected. </p>
+            </section>
+
+            <section>
+                <h2>Unspoken Rules </h2>
+                <p>There are several unspoken rules when it comes to attending the esteemed tournament. The players are dress coded so they cannot wear anything that isn't white. 
+                Oftentimes, the audience is meant to follow the same dresscode. You are not meant to cheer as soon as a player begins their serve. Wimbledon is also not the place to get drunk.
+                Not only is it very expenisve, many people will be removed if there is any type of disorderly behavior. Lastly, it is highly frowned upon to have dirty sneakers on, especially in the Centre Courts.</p>
+            </section>
+
+            
+
+            <section>
+                <h2>Official Logo</h2>
+                <figure>
+                    <img class ="rounded" alt ="Logo" src="images/logo.png"> 
+                    <figcaption><cite><a href="https://www.pinterest.co.uk/pin/846747167421531213/"
+                    target="_blank"></a></cite>The Championships</figcaption>
+                </figure>
+            </section> 
+
+        </main>
+
+        <footer>
+            <p><a href="../index.php">Site Map (Main index)</a></p>
+        </footer>
+    </body>
+</html>
---
+++ lab6/form.php
@@ -0,0 +1,141 @@
+<!DOCTYPE HTML>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title> Tennis </title>
+        <meta name="author" content="Tushar Asthana">
+        <meta name="description" content="The 4th largest followed sport in the world. Players from all over the world come to 
+        compete in tournaments to make a name for themselves. Here we will break down rankings, icons, and my favorite player! ">
+        <link rel="stylesheet" href="css/custom.css?version=<?php print time(); ?>" type="text/css">
+        <!-- Side note to myself, things come and go, take a breath. -->
+        <meta name ="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="css/custom.css?version=<?php print time(); ?>" ype="text/css">
+        <link rel="stylesheet" media= "(max-width: 800px)" href="css/custom-tablet.css?version=<?php print time(); ?>"type="text/css">
+        <link rel="stylesheet" media=" (max-width: 600px)" href="css/custom-phone.css?version=<?php print time(); ?>" type="text/ass">
+    </head>
+
+    <body>
+
+        <header>
+        <!-- logo image would go here -->
+            <h1>Form</h1>
+            <h2>How much of a tennis nerd are you?</h2>
+        </header>
+
+        <nav class="nav">
+            <a href="index.php">Home</a>
+            <a href="detail.php">Wimbledon</a>
+            <a href="form.php">Form</a>
+        </nav>
+
+        <main>
+        <h1>Survey</h1>
+
+            <section>
+                <h2>Judging everyone based off of their favorite tennis player of the past decade.</h2>
+                <p> Answer these few questions for a judgement!</p>
+            </section>
+
+            <section>
+                <h2>This information will let me know whose likely to be my friend.</h2>
+                <form action="#" id= "frmFavPlayer" method="post">
+
+                    <fieldset class="listbox">
+                        <legend>Favorite Player</legend>
+                        <p>
+                            <select id="lstFavPlayer" name="lstFavPlayer" tabindex="120">
+                                <option value="Roger Federer">Roger Federer</option>
+                                <option value="Serena Williams">Serena Williams</option>
+                                <option value="Novak Djokovic">Novak Djokovic</option>
+                                <option value="Andy Murray">Andy Murray</option>
+                                <option value="Rafael Nadal<">Rafael Nadal</option>
+                                <option value="Venus Williams">Venus Williams</option>
+                                <option value="Carlos Alcaraz">Carlos Alcaraz</option>
+                                <option value="Casper Rudd">Casper Rudd</option>
+                            </select>
+                        </p>
+                    </fieldset> 
+
+                    <fieldset class="contact">
+                        <legend>What's your email?</legend>
+                        <p>
+                            <label class="required" for="txtEmail" >Email</label>
+                            <input id="txtEmail" maxlength="50" name="txtEmail"
+                            onfocus="this.select()" tabindex="310" type="text" value=""
+                            required>
+                        </p>
+                    </fieldset>
+
+                    <fieldset class="checkbox">
+                        <legend>What kind of player are you?</legend>
+                        <p>
+                            <input id="chkRock" name="chkRock" tabindex="510"
+                            type="checkbox" value="1">
+                            <label for="chkRock">Rock</label>
+                        </p>
+
+                        <p>
+                            <input id="chkPaper" name="chkPaper" tabindex="520"
+                            type="checkbox" value="1">
+                            <label for="chkPaper">Paper</label>
+                        </p>
+
+                        <p>
+                            <input id="chkScissor" name="chkScissor" tabindex="530"
+                            type="checkbox" value="1">
+                            <label for="chkScissor">Scissor</label>
+                        </p>
+                    </fieldset>
+
+                    <fieldset class="radio">
+                        <legend>How often do you play tennis?</legend>
+                        <p>
+                            <input type="radio" id="radCoupleDays"
+                            name="radPick" value="Couple" tabindex="410" required>
+                            <label class="radio-field" for="radCoupleDays">Couple Days</label>
+                        </p>
+
+                        <p>
+                            <input type="radio" id="radFewDays"
+                            name="radPick" value="Few" tabindex="410" required>
+                            <label class="radio-field" for="radFewDays">Few Days</label>
+                        </p>
+
+                        <p>
+                            <input type="radio" id="radEveryDays"
+                            name="radPick" value="Every" tabindex="410" required>
+                            <label class="radio-field" for="radEveryDays">Every Day</label>
+                        </p>
+
+                        <fieldset class="button">
+                            <input id="btnSubmit" name="btnSubmit" tabindex="900"
+                            type="submit" value="Submit">
+                        </fieldset>
+                    </fieldset> 
+
+                </form>
+            </section>
+
+            <section>
+                <h2>Favorite Tennis Picture</h2>
+                <figure class="go-right">
+                    <img class="rounded" alt ="Big Four" src="images/fed.jpg">
+                    <figcaption><cite><a href= "" target="_blank"></a></cite>Roger Federer's Instagram</figcaption>
+                </figure>
+            </section> 
+
+            <section>
+            <h2>Appreciate it</h2>
+            <?php
+            print'<p>Post Array:</p><pre>';
+            print_r($_POST);
+            print '</pre>';
+            ?>
+            </section> 
+        </main>
+        
+        <footer>
+            <p><a href="../index.php">Site Map (Main index)</a></p>
+        </footer>
+    </body>
+</html>
