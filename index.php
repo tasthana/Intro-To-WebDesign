78f862627bcecbb63f8d891b58f79617f150d08a
Tushar Asthana <101343079+tasthana@users.noreply.github.com>
2022-11-08 09:12:28 -0500

    Lab 8: updated site map

--- index.php
+++ index.php
@@ -88,6 +88,42 @@
         <h1>CS 008-<em>B</em> Fall 2022</h1>
         <h2>Tushar Asthana - Site map</h2>
         <p><a href="ADMIN/admin.php">My Admin Folder</a></p>
+        <p><a href="ADMIN/table-viewer.php?getDatabase=">Database Viewer</a></p>
+        
+        <section class="grid-layout">
+            <h2 class="header">Lab Eight - Database to display data.</h2>
+            <section class="public-files">
+                <h3>Public Files</h3>
+                <p><a href="lab8/array.php">array.php</a></p>
+                <p><a href="lab8/detail.php">detail.php</a></p> 
+                <p><a href="lab8/form.php">form.php</a></p> 
+                <p><a href="lab8/index.php">index.php</a></p>                
+            </section>
+
+            <section class="supporting-files">
+                 <h3>Supporting files</h3>
+                <p><a href="lab8/css/custom.css">custom.css</a> </p>
+                <p><a href="lab8/css/custom-phone.css">custom-phone.css</a> </p>
+                <p><a href="lab8/css/custom-tablet.css">custom-tablet.css</a> </p>
+                
+                <p><a href="lab8/sql.php">DB SQL Statements</a> </p>
+                <p><a href="lab8/records.png">DB records</a> </p>
+                
+                <p><a href="lab8/footer.php">footer.php</a></p>
+                <p><a href="lab8/header.php">header.php</a></p>
+                <p><a href="lab8/nav.php">nav.php</a></p>
+                <p><a href="lab8/top.php">top.php</a></p>
+                
+                <p><a href="lab8/images/wireframe.png">wireframe desktop</a></p>
+                <p><a href="lab8/images/phone-wireframe.png">wireframe phone</a></p>
+                <p><a href="lab8/images/wireframe-tablet.png">wireframe tablet</a></p>
+            </section>
+
+            <section class="grader-notes">
+                <h3>Notes to grader</h3>
+                <p></p>
+            </section>
+        </section>
         
         <section class="grid-layout">
             <h2 class="header">Lab Seven - Arrays and includes.</h2>
