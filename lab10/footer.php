b52f4a7650ec9a3d2ff7e756590969984b197157
Tushar Asthana <101343079+tasthana@users.noreply.github.com>
2022-12-13 10:48:35 -0500

    Final Project: created template

---
+++ lab10/footer.php
@@ -0,0 +1,7 @@
+
+<footer>
+    <p><a href="../index.php">Site Map</a></p>
+    <p><a href="https://tasthana.w3.uvm.edu/cs008/lab10/index.php">Final Project</a></p>
+    
+</footer>
+    
---
+++ lab10/header.php
@@ -0,0 +1,5 @@
+<header>
+    <!-- logo image would go here -->
+    <h1>Travel with Tushar </h1>
+    <h2>A review of the places I have seen!</h2>
+</header>
